97f7769e9eb51ed992d224b020cb2fb71237fa89
Simon Kjellberg <simon.kjellberg@gmail.com>
2018-07-05 09:42:44 +0200

    feat: add sitemap generator

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,14 @@
 
 module.exports = {
   siteMetadata: {
+    siteUrl: 'https://shebang.consulting',
     title: 'Shebang Consulting',
     description:
       'Fullstack web consulting, specialized in React, Node.js, and GraphQL, with a strong focus on building scalable frontend architecture.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-emotion',
     'gatsby-plugin-offline',
     'gatsby-plugin-netlify',
