e332168bfee47483748f9364d02d4db2ae1ad870
Simon Kjellberg <simon.kjellberg@gmail.com>
2018-09-18 15:48:00 +0200

    fix: filter out manifest plugin favicon

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,15 @@
+'use strict'
+
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  replaceHeadComponents(
+    getHeadComponents().filter(
+      el => el.key !== 'gatsby-plugin-manifest-icon-link'
+    )
+  )
+}
